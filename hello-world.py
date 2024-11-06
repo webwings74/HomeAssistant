b994ad22cdb2726dcd86171833060c8fee754912
Richard <rvdveldt@gmail.com>
2024-11-06 11:47:01 +0100

    hello-world comment

--- hello-world.py
+++ hello-world.py
@@ -1,4 +1,8 @@
+# Tot nu toe heb ik geen werkende scripts kunnen maken met Python onder
+# HomeAssistant. Het blijft een enigma.
+
 # `data` is available as builtin and is a dictionary with the input data.
 name = data.get("name", "world")
+
 # `logger` and `time` are available as builtin without the need of explicit import.
 logger.info("Hello {} at {}".format(name, time.time()))
